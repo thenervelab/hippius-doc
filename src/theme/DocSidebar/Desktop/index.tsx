4389febb559b1b90378007eb1cc33e480dcba128
Akin Aguda <lordtitan7@gmail.com>
2025-03-13 16:55:58 +0100

    docs scroll fix

--- src/theme/DocSidebar/Desktop/index.tsx
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -22,10 +22,10 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
         styles.sidebar,
         hideOnScroll && styles.sidebarWithHideableNavbar,
         isHidden && styles.sidebarHidden,
-        "pl-4 pb-4 border-none"
+        "pl-4 pb-4 border-none flex flex-col"
       )}
     >
-      <div className="border mt-4 pt-2 px-2 flex grow border-grey-80 rounded">
+      <div className="border mt-4 pt-2 px-2 pb-4 flex grow border-grey-80 rounded overflow-y-auto">
         {hideOnScroll && <Logo tabIndex={-1} className={styles.sidebarLogo} />}
         <Content className="p-0" path={path} sidebar={sidebar} />
         {hideable && <CollapseButton onClick={onCollapse} />}
