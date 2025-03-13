73a1e54470317804ca49a0b964ab3b4041dbad0e
Akin Aguda <lordtitan7@gmail.com>
2025-03-13 12:40:58 +0100

    Styled footer

---
+++ src/theme/Footer/Layout/index.tsx
@@ -0,0 +1,37 @@
+import React, { type ReactNode } from "react";
+import Graphsheet from "@site/src/components/graphsheet";
+import type { Props } from "@theme/Footer/Layout";
+
+export default function FooterLayout({
+  style,
+  links,
+  logo,
+  copyright,
+}: Props): ReactNode {
+  return (
+    <footer className="relative bg-primary-50 py-14 px-6 flex justify-center">
+      <Graphsheet
+        majorCell={{
+          lineColor: [255, 255, 255, 1.0],
+          lineWidth: 3,
+          cellDim: 70,
+        }}
+        minorCell={{
+          lineColor: [255, 255, 255, 0.0],
+          lineWidth: 0.0,
+          cellDim: 5,
+        }}
+        className="absolute w-full h-full top-0 opacity-5"
+      />
+      <div className="relative w-full max-w-screen-xl">
+        {links}
+        {(logo || copyright) && (
+          <div className="flex font-medium">
+            {logo && <div>{logo}</div>}
+            {copyright}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
