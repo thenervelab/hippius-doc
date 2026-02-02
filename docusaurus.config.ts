f3914dd4d72f27a0ef7409e1c6844f0f1a6c9f89
raoarhamfaisal <arhamfaisal780@gmail.com>
2026-02-02 15:18:46 +0500

    feat: update copyright line in footer to use copyright icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -254,7 +254,13 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Hippius.`,
+      copyright: `<span class="flex items-center gap-x-2">
+        <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="size-5">
+          <path d="M10 18.3332C5.40001 18.3332 1.66667 14.5998 1.66667 9.99984C1.66667 5.39984 5.40001 1.6665 10 1.6665C14.6 1.6665 18.3333 5.39984 18.3333 9.99984C18.3333 14.5998 14.6 18.3332 10 18.3332Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12.4 12.4998C11.8083 13.0165 11.0417 13.3332 10.2 13.3332C8.35832 13.3332 6.86665 11.8415 6.86665 9.99984C6.86665 8.15817 8.35832 6.6665 10.2 6.6665C11.0417 6.6665 11.8083 6.98317 12.4 7.49984" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        Hippius ${new Date().getFullYear()}
+      </span>`,
     },
     prism: {
       theme: prismThemes.dracula,
